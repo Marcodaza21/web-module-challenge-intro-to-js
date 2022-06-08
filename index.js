636f1119ef07291a8842414ba7e9bdf898022a3b
Marco Daza <marco@Marcos-MacBook-Pro.local>
2022-06-08 00:08:39 -0600

    Finished

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(a,b){
+  return a*b;
 }
 
 
@@ -77,8 +77,8 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(h){
+  return h*7;
 }
 
 
@@ -129,8 +129,26 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight,age){
+  if (age >= 1){
+    if (weight <= 5){
+      return weight * 0.05
+    } else if (weight <= 10){
+      return weight * 0.04
+    } else if ( weight <= 15){
+      return weight * 0.03
+    } else if ( weight > 15){
+      return weight * 0.02
+    }
+  } if (age < 1){
+    if (age <= 0.33){
+      return weight * 0.10
+    } else if (age <= 0.58){
+      return weight * 0.05
+    } else if ( age <= 1){
+      return weight * 0.04
+    }
+  }
 }
 
 
@@ -156,8 +174,26 @@ Use the game function below to do the following:
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
 
-function game(user, computer){
-  /*add your code here*/
+function game(user,computer){
+  if (user === "scissors" && computer === "paper"){
+    return "you win!"
+  } else if (user === "scissors" && computer === "rock"){
+    return "you lose!"
+  } else if (user === "scissors" && computer === "scissors"){
+    return "it's a tie!"
+  } else if (user === "rock" && computer === "scissors"){
+    return "you win!"
+  } else if (user === "rock" && computer === "paper"){
+    return "you lose!"
+  } else if (user === "rock" && computer === "rock"){
+    return "it's a tie"
+  } else if (user === "paper" && computer === "rock"){
+    return "you win!"
+  } else if (user === "paper" && computer === "scissors"){
+    return "you lose!"
+  } else if (user === "paper" && computer === "paper"){
+    return "it's a tie"
+  }
 }
 
 
@@ -173,8 +209,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(KM){
+  return KM * 0.621371
 }
 
 
@@ -187,8 +223,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm /30.48
 }
 
 
@@ -206,12 +242,13 @@ Using the annoyingSong function below do the following:
 3. Outside of the function, Make a loop that invokes annoying song with a number that decreases until it gets to 1 bottle left. 
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
-
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+// for loop!
+function annoyingSong(number){
+  for (let i = number; i > 1; i-1){
+    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`
+  }
 }
-
-
+  // console.log(annoyingSong)
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 //Grade Calculator
@@ -227,8 +264,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if (score >=90){
+    return 'you got an A'
+  } else if (score >=80){
+    return 'you got a B'
+  } else if (score >=70){
+    return 'you got a C'
+  } else if (score >=60){
+    return 'you got a D'
+  } else if (score >=0){
+    return 'you got an F'
+  }
 }
 
 
